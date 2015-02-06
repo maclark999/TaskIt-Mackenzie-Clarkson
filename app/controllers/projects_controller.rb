e92f026171d0d556438a14f8b331c2b2660dd1f5
mackenzie clarkson <mac3@mackenzies-MacBook-Pro.local>
2015-02-05 21:41:59 -0700

    add ability to crud projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update, :destroy]
+  def index
+    @projects = Project.all
+  end
+
+  def show
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: 'User was successfully created'
+    else
+      render ('new')
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+      if @project.destroy
+        redirect_to projects_path, notice: 'User was successfully destroyed'
+      end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @project = Project.find(params[:id])
+      if @project.update_attributes(project_params)
+        redirect_to project_path, notice: 'User was successfully updated'
+      else render ('index')
+      end
+  end
+
+  private
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+
+    def project_params
+      params.require(:project).permit(:name)
+    end
+end
