c4d84c01b8d6a1c3dbe6b8370c18711db38356a8
mackenzie clarkson <maclark99@gmail.com>
2015-02-21 15:17:09 -0700

    user can click on name in nav bar and see his or her show page, user can sign out

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, notice: 'Welcome to gCamp!'
     else
       render 'new'
     end
