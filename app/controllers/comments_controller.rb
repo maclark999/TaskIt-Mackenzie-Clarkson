39e3b1f4f27221bf68c468efb314e2a913681166
mackenzie clarkson <maclark99@gmail.com>
2015-04-07 13:47:08 -0600

    assign user_id and task_id to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,11 @@
 class CommentsController < ApplicationController
-  # def new
-  #   @task = Task.find(params[:task_id])
-  #   @project = Project.find(params[:project_id])
-  #   @comment = Comment.new
-  # end
 
   def create
     @task = Task.find(params[:task_id])
     @project = @task.project_id
     @comment = @task.comments.build(comment_params)
     @comment.task_id = params[:task_id]
-    @comment.user_id = params[:user_id]
+    @comment.user_id = session[:user_id]
       if @comment.save
         redirect_to project_task_path(@project, @task)
       end
